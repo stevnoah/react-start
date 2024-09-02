9eca95912e94d602b2e8bf7910cd5132f66c7baf
Ojodunwene Stephen Noah <ojodunwene.m2100914@st.futminna.edu.ng>
2024-09-02 15:25:16 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import ProTip from './ProTip';
+import Index from './components/router';
+
+function Copyright() {
+  return (
+    <Typography
+      variant="body2"
+      align="center"
+      sx={{
+        color: 'text.secondary',
+      }}
+    >
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
+  );
+}
+
+export default function App() {
+  return (
+    <Index />
+  );
+}
